0c176a801df508e6e030f76a7ae485e69336eb77
Anders Østby <anders.ostby@nav.no>
2020-05-06 16:06:33 +0200

    Lagt til testhendelse i EndToEndTest

--- src/test/kotlin/no/nav/helse/EndToEndTest.kt
+++ src/test/kotlin/no/nav/helse/EndToEndTest.kt
@@ -23,9 +23,11 @@ import java.net.HttpURLConnection
 import java.net.ServerSocket
 import java.net.Socket
 import java.net.URL
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.streams.asSequence
 import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -352,3 +354,60 @@ private fun vedtakMedUtbetalingslinjernøkkel(fom: LocalDate, tom: LocalDate) = 
       "system_read_count": 0
     }
 """
+
+@Language("JSON")
+private fun vedtakV3(fom: LocalDate, tom: LocalDate) = """{
+    "aktørId": "aktørId",
+    "fødselsnummer": "fnr",
+    "organisasjonsnummer": "orgnummer",
+    "hendelser": [
+        "7c1a1edb-60b9-4a1f-b976-ef39d4d5021c",
+        "798f60a1-6f6f-4d07-a036-1f89bd36baca",
+        "ee8bc585-e898-4f4c-8662-f2a9b394896e"
+    ],
+    "utbetalt": [
+        {
+            "mottaker": "orgnummer",
+            "fagområde": "SPREF",
+            "fagsystemId": "77ATRH3QENHB5K4XUY4LQ7HRTY",
+            "førsteSykepengedag": "",
+            "totalbeløp": 8586,
+            "utbetalingslinjer": [
+                {
+                    "fom": "$fom",
+                    "tom": "$tom",
+                    "dagsats": 1431,
+                    "beløp": 1431,
+                    "grad": 100.0,
+                    "sykedager": ${sykedager(fom, tom)}
+                }
+            ]
+        },
+        {
+            "mottaker": "fnr",
+            "fagområde": "SP",
+            "fagsystemId": "353OZWEIBBAYZPKU6WYKTC54SE",
+            "totalbeløp": 0,
+            "utbetalingslinjer": []
+        }
+    ],
+    "fom": "$fom",
+    "tom": "$tom",
+    "forbrukteSykedager": ${sykedager(fom, tom)},
+    "gjenståendeSykedager": ${248 - sykedager(fom, tom)},
+    "opprettet": "2020-05-04T11:26:30.23846",
+    "system_read_count": 0,
+    "@event_name": "utbetalt",
+    "@id": "e8eb9ffa-57b7-4fe0-b44c-471b2b306bb6",
+    "@opprettet": "2020-05-04T11:27:13.521398",
+    "@forårsaket_av": {
+        "event_name": "behov",
+        "id": "cf28fbba-562e-4841-b366-be1456fdccee",
+        "opprettet": "2020-05-04T11:26:47.088455"
+    }
+}
+"""
+
+private fun sykedager(fom: LocalDate, tom: LocalDate) =
+    fom.datesUntil(tom.plusDays(1)).asSequence()
+        .filter { it.dayOfWeek !in arrayOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY) }.count()
