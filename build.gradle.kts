204aa87ee317b7bfcbf49e2482bc798a9048b886
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-07-10 09:58:47 +0200

    Bumper dependency versjoner og bygger med JDK14

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val junitJupiterVersion = "5.5.2"
+val junitJupiterVersion = "5.6.2"
 val ktorVersion = "1.3.2"
-
-val kafkaVersion = "2.3.1"
-val rapidsAndRiversVersion = "1.74ae9cb"
-
-val wireMockVersion = "2.19.0"
+val rapidsAndRiversVersion = "fa839faa1c"
+val wireMockVersion = "2.27.1"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -21,13 +18,7 @@ repositories {
     mavenCentral()
     maven("https://packages.confluent.io/maven/")
     maven("https://kotlin.bintray.com/ktor")
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
-    }
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -41,11 +32,10 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
     testImplementation("no.nav:kafka-embedded-env:2.4.0")
-    testImplementation("org.awaitility:awaitility:4.0.1")
+    testImplementation("org.awaitility:awaitility:4.0.3")
 
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
-    testImplementation ("com.nimbusds:nimbus-jose-jwt:7.5.1")
     testImplementation("com.github.tomakehurst:wiremock:$wireMockVersion") {
         exclude(group = "junit")
     }
