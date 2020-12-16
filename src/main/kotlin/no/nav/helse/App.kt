11058311f4443e81c268ff7caccea2f0be6e37b1
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2020-12-16 17:10:56 +0100

    Leser inn utbetaling_utbetalt på nytt i parallell river
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -14,11 +14,17 @@ import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.jackson.*
 import io.ktor.routing.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import no.nav.helse.rapids_rivers.KafkaConfig
+import no.nav.helse.rapids_rivers.KafkaRapid
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileNotFoundException
 import java.net.URL
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -41,6 +47,8 @@ fun main() {
 
     val vedtakproducer = KafkaProducer<String, Vedtak>(loadBaseConfig(environment, serviceUser).toProducerConfig())
 
+    seedRapid(System.getenv(), vedtakproducer, environment.vedtaksfeedtopic)
+
     RapidApplication.Builder(
         RapidApplication.RapidApplicationConfig.fromEnv(System.getenv())
     ).withKtorModule {
@@ -51,6 +59,32 @@ fun main() {
         start()
     }
 }
+
+private fun seedRapid(env: Map<String, String>, vedtakproducer: KafkaProducer<String, Vedtak>, vedtaksfeedTopic: String) {
+    val kafkaConfig = KafkaConfig(
+        bootstrapServers = env.getValue("KAFKA_BOOTSTRAP_SERVERS"),
+        consumerGroupId = env.getValue("KAFKA_CONSUMER_GROUP_ID") + "-reseed",
+        username = "/var/run/secrets/nais.io/service_user/username".readFile(),
+        password = "/var/run/secrets/nais.io/service_user/password".readFile(),
+        truststore = env["NAV_TRUSTSTORE_PATH"],
+        truststorePassword = env["NAV_TRUSTSTORE_PASSWORD"],
+        autoOffsetResetConfig = "earliest"
+    )
+    val seedRapid = KafkaRapid.create(kafkaConfig, env.getValue("KAFKA_RAPID_TOPIC"))
+        .apply {
+            Runtime.getRuntime().addShutdownHook(Thread(this::stop))
+            UtbetalingUtbetaltRiver(this, vedtakproducer, vedtaksfeedTopic)
+        }
+
+    GlobalScope.launch { seedRapid.start() }
+}
+
+private fun String.readFile() =
+    try {
+        File(this).readText(Charsets.UTF_8)
+    } catch (err: FileNotFoundException) {
+        null
+    }
 
 internal fun Application.vedtaksfeed(
     environment: Environment,
