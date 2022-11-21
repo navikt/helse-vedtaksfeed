5694fef8714b66d519736f77751f7354ddc19665
David Steinsland <david@steinsland.email>
2022-11-21 14:54:47 +0100

    tester egen app i dev-gcp med azure app auth

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -3,6 +3,7 @@ package no.nav.helse
 import com.auth0.jwk.JwkProvider
 import com.auth0.jwk.JwkProviderBuilder
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -12,8 +13,10 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.cio.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import no.nav.helse.rapids_rivers.KtorBuilder
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -23,6 +26,8 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.InputStream
+import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.TimeUnit
 
@@ -36,11 +41,6 @@ val log: Logger = LoggerFactory.getLogger("vedtaksfeed")
 fun main() {
     val environment = setUpEnvironment()
 
-    val jwkProvider = JwkProviderBuilder(URL(environment.jwksUrl))
-        .cached(10, 24, TimeUnit.HOURS)
-        .rateLimited(10, 1, TimeUnit.MINUTES)
-        .build()
-
     val env = System.getenv()
     val aivenConfig = KafkaConfig(
         bootstrapServers = env.getValue("KAFKA_BROKERS"),
@@ -49,6 +49,15 @@ fun main() {
         keystoreLocation = env["KAFKA_KEYSTORE_PATH"],
         keystorePassword = env["KAFKA_CREDSTORE_PASSWORD"],
     )
+
+    if (true == System.getenv("KUN_KTOR_API")?.equals("true", true)) {
+        return apiOnly(env, environment, aivenConfig)
+    }
+
+    val jwkProvider = JwkProviderBuilder(URL(environment.jwksUrl))
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
 
     RapidApplication.Builder(
         RapidApplication.RapidApplicationConfig.fromEnv(System.getenv())
@@ -61,6 +70,35 @@ fun main() {
             vedtakaivenProducer.send(ProducerRecord(environment.vedtaksfeedtopic, fødselsnummer, vedtak)).get().offset()
         }
     }
+}
+
+private fun apiOnly(env: Map<String, String>, environment: Environment, aivenConfig: KafkaConfig) {
+    val azureConfig = AzureAdAppConfig(
+        clientId = env.getValue("AZURE_APP_CLIENT_ID"),
+        configurationUrl = env.getValue("AZURE_APP_WELL_KNOWN_URL")
+    )
+    val ktor = KtorBuilder()
+        .log(log)
+        .port(8080)
+        .liveness { true }
+        .readiness { true }
+        .module {
+            installJacksonFeature()
+            install(Authentication) {
+                jwt {
+                    azureConfig.configureVerification(this)
+                }
+            }
+
+            routing {
+                authenticate {
+                    feedApi(environment.vedtaksfeedtopic, KafkaConsumer(aivenConfig.consumerConfig(), StringDeserializer(), VedtakDeserializer()))
+                }
+            }
+        }
+    val app = ktor.build(CIO)
+    Runtime.getRuntime().addShutdownHook(Thread { app.start(wait = false) })
+    app.start(wait = true)
 }
 
 internal fun RapidsConnection.setupRivers(publisher: Publisher) {
@@ -106,5 +144,39 @@ internal fun Application.installJacksonFeature() {
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             registerModule(JavaTimeModule())
         }
+    }
+}
+
+internal class AzureAdAppConfig(private val clientId: String, configurationUrl: String) {
+    private val issuer: String
+    private val jwkProvider: JwkProvider
+    private val jwksUri: String
+
+    init {
+        configurationUrl.getJson().also {
+            this.issuer = it["issuer"].textValue()
+            this.jwksUri = it["jwks_uri"].textValue()
+        }
+
+        jwkProvider = JwkProviderBuilder(URL(this.jwksUri)).build()
+    }
+
+    fun configureVerification(configuration: JWTAuthenticationProvider.Config) {
+        configuration.verifier(jwkProvider, issuer) {
+            withAudience(clientId)
+        }
+        configuration.validate { credentials -> JWTPrincipal(credentials.payload) }
+    }
+
+    private fun String.getJson(): JsonNode {
+        val (responseCode, responseBody) = this.fetchUrl()
+        if (responseCode >= 300 || responseBody == null) throw RuntimeException("got status $responseCode from ${this}.")
+        return jacksonObjectMapper().readTree(responseBody)
+    }
+
+    private fun String.fetchUrl() = with(URL(this).openConnection() as HttpURLConnection) {
+        requestMethod = "GET"
+        val stream: InputStream? = if (responseCode < 300) this.inputStream else this.errorStream
+        responseCode to stream?.bufferedReader()?.readText()
     }
 }
