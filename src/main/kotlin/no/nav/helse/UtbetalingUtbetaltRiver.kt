d108908237b24d2b0af096003dbc17d6caa09414
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2024-02-20 09:44:06 +0100

    Flytt rivers ut av Rivers
    Konvensjonen er å ha en fil for hver river-implementasjon

--- src/main/kotlin/no/nav/helse/UtbetalingUtbetaltRiver.kt
+++ src/main/kotlin/no/nav/helse/UtbetalingUtbetaltRiver.kt
@@ -90,61 +90,6 @@ class UtbetalingUtbetaltRiver(
 
 }
 
-class AnnullertRiverV1(
-    rapidsConnection: RapidsConnection,
-    private val vedtaksfeedPublisher: Publisher
-) : River.PacketListener {
-
-    init {
-        River(rapidsConnection).apply {
-            validate {
-                it.demandValue("@event_name", "utbetaling_annullert")
-                it.require("@opprettet", JsonNode::asLocalDateTime)
-                it.requireKey(
-                    "fødselsnummer",
-                    "aktørId",
-                    "organisasjonsnummer",
-                    "utbetalingId",
-                    "korrelasjonsId",
-                    "fom",
-                    "tom"
-                )
-            }
-        }.register(this)
-    }
-
-    override fun onError(problems: MessageProblems, context: MessageContext) {
-        tjenestekallLog.error("Forstod ikke innkommende melding (utbetaling_annullert): ${problems.toExtendedReport()}")
-        log.error("Forstod ikke innkommende melding (utbetaling_annullert): $problems")
-    }
-
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        try {
-            val utbetalingId = packet["utbetalingId"].asText()
-            val (korrelasjonsId, base32EncodedKorrelasjonsId) = packet.korrelasjonsId()
-            val fom = packet["fom"].asLocalDate()
-            val tom = packet["tom"].asLocalDate()
-            val offset = Vedtak(
-                type = Vedtak.Vedtakstype.SykepengerAnnullert_v1,
-                opprettet = packet["@opprettet"].asLocalDateTime(),
-                aktørId = packet["aktørId"].textValue(),
-                fødselsnummer = packet["fødselsnummer"].asText(),
-                førsteStønadsdag = fom,
-                sisteStønadsdag = tom,
-                førsteFraværsdag = base32EncodedKorrelasjonsId,
-                forbrukteStønadsdager = 0
-            ).republish(vedtaksfeedPublisher)
-            "Republiserer annullering for utbetalingId=$utbetalingId og korrelasjonsId=$korrelasjonsId ($base32EncodedKorrelasjonsId) på intern topic med offset $offset".also {
-                log.info(it)
-                tjenestekallLog.info(it)
-            }
-        } catch (e: Exception) {
-            tjenestekallLog.error("Melding feilet ved konvertering til internt format:\n${packet.toJson()}")
-            throw e
-        }
-    }
-}
-
 private fun JsonMessage.korrelasjonsId() = UUID.fromString(get("korrelasjonsId").textValue()).let { korrelasjonsId ->
     korrelasjonsId to korrelasjonsId.base32Encode()
 }
@@ -160,5 +105,4 @@ private fun JsonMessage.forbrukteStønadsdager(): Int {
 }
 
 private fun Vedtak.republish(publisher: Publisher) = publisher(fødselsnummer, this)
-
 
