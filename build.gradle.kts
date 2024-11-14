12a3f72d8c7119af66e747daa3a193592173746c
david steinsland <david@davidsteinsland.net>
2024-11-14 08:45:11 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val junitJupiterVersion = "5.11.3"
 val ktorVersion = "3.0.1"
-val rapidsAndRiversVersion = "2024111220531731441232.6f0a7a6c643b"
+val rapidsAndRiversVersion = "2024111408081731568123.6619b48b7043"
 val tbdLibsVersion = "2024.11.13-14.25-4e975e00"
 val wireMockVersion = "3.0.3"
 val mockkVersion = "1.13.13"
