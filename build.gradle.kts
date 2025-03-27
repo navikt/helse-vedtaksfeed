b7dde01ea50a18eddf7f2154ee23efe0f25a5447
david steinsland <david@davidsteinsland.net>
2025-03-27 18:08:11 +0100

    Bump Kotlin JVM -> 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val tbdLibsVersion = "2025.01.27-12.50-76316f3b"
 val wireMockVersion = "3.9.2"
 val mockkVersion = "1.13.13"
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.20"
 }
 
 repositories {
