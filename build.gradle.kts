c4c04fa5b58f74b9d3793a314c468431f6dbeaac
david steinsland <david@davidsteinsland.net>
2025-08-07 11:46:18 +0200

    bumper tbd-libs og r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val junitJupiterVersion = "5.12.1"
-val ktorVersion = "3.1.2"
-val rapidsAndRiversVersion = "2025061811051750237542.df739400e55e"
-val tbdLibsVersion = "2025.04.04-09.18-7cc3badf"
+val ktorVersion = "3.2.3"
+val rapidsAndRiversVersion = "2025080710011754553680.051be9b54ef9"
+val tbdLibsVersion = "2025.08.07-09.55-15719a86"
 val mockkVersion = "1.13.17"
 plugins {
     kotlin("jvm") version "2.2.0"
